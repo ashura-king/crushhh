9df96b8fc4306163df3b90c6c8ed35fa1f2e5dc9
ashura-king <izanagire34@gmail.com>
2025-04-20 18:55:35 +0800

    Add files via upload

---
+++ Confess-Crush/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Coffess",
+    introTitle: 'Alexa Aljeina Perdigones',
+    introDesc: 'The Earth is full of wonders\nBut why do you keep getting lost?\nLost into my lonely heart\nBehind the word love is affection.',
+    btnIntro: 'HiHi',
+    title: 'Could it be that you fell in love at first sight 🥰',
+    desc: 'Could it be that you were enchanted from the moment you saw that smile?',
+    btnYes: 'Love it so much <33',
+    btnNo: 'Nope :3',
+    question: 'Out of more than 7 billion people in the world, why do you love me? <3',
+    btnReply: 'Send to you <3',
+    reply: 'Love is love, and even if it’s not, it’s still love <33333333',
+    mess: 'I knew it 🥰. Love you so so much 😘😘',
+    messDesc: 'Tommorow at 12:00, I’ll come pick you up for a lunch.',
+    btnAccept: 'Okiiiii always <3',
+    messLink: 'https://github.com/ashura-king/Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
